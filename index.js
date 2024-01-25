fefc39397eeae6ee7e5d4e99bdfc09520c60b512
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-01-26 00:33:13 +0600

    added the deposit collection and api route

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const {
+  MongoClient,
+  ServerApiVersion
+} = require('mongodb');
 const app = express();
 require("dotenv").config();
+// const bodyParser = require('body-parser');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
-
-
+// app.use(bodyParser.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pwyhut1.mongodb.net/?retryWrites=true&w=majority`;
@@ -32,18 +36,72 @@ async function run() {
     // mongodb collections
     const assetsCollection = nexTrade.collection('assets');
     const usersCollection = nexTrade.collection('all-users');
+    const walletCollection = nexTrade.collection('wallet');
 
 
+    // stripe //
+    app.post('/create-payment-intent', async (req, res) => {
+      try {
+        const {
+          price
+        } = req.body;
 
-    
+        if (price === 0) {
+
+        }
+        const amount = price ? parseInt(price * 100) : 50;
+
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card'],
+        });
+
+        res.send({
+          clientSecret: paymentIntent.client_secret
+        });
+      } catch (error) {
+        console.error("Error creating payment intent:", error);
+        res.status(500).send({
+          error: "Error creating payment intent."
+        });
+      }
+    });
+
+    app.post('api/v1/deposit', async (req, res) => {
+      // const email = req.params.email;
+      const depositData = req.body;
+      // const query = {
+      //   email: email
+      // };
+      // const depositInfo = {
+      //   $set: {
+      //     transaction: depositData.paymentIntent,
+      //     date: depositData.date,
+      //     time: depositData.time,
+      //     deposit: depositData.amount,
+      //     email: depositData.user.email,
+      //     name: depositData.user.displayName,
+      //   }
+      // }
+      const result = await walletCollection.insertOne(depositData)
+      console.log(result);
+      res.send(result)
+    })
+
     // user related api starts form here
 
     // post a user in usersCollection
     app.post('/v1/api/all-users', async (req, res) => {
       const userInfo = req.body;
-      const existingUser = await usersCollection.findOne({ email: userInfo.email })
+      const existingUser = await usersCollection.findOne({
+        email: userInfo.email
+      })
       if (existingUser) {
-        return res.send({ message: 'user already exists', insertedId: null })
+        return res.send({
+          message: 'user already exists',
+          insertedId: null
+        })
       }
       const result = await usersCollection.insertOne(userInfo);
       res.send(result)
@@ -51,7 +109,9 @@ async function run() {
 
     // get all users from usersCollection
     app.get('/v1/api/all-users', async (req, res) => {
-      const result = await usersCollection.find().sort({ _id: -1 }).toArray();  // get users in lifo methods
+      const result = await usersCollection.find().sort({
+        _id: -1
+      }).toArray(); // get users in lifo methods
       res.send(result)
     })
 
@@ -78,7 +138,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({
+      ping: 1
+    });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
