ed899a8d5883fc406d33f6313798365d1de2bdc9
mahindipto <mahindipto18@gmail.com>
2024-03-07 17:13:05 +0600

    updated patch api

--- index.js
+++ index.js
@@ -1326,19 +1326,20 @@ async function run() {
 
     //  ========== spotTrading collection APIs ========== //
         // update user balance
-        app.patch("/v1/api/all-users/:email/:balance", async (req, res) => {
+        app.patch("/v1/api/all-users/:email", async (req, res) => {
           const userEmail = req.params.email;
-          const userBalance = req.params.balance;
+          const userBalance = req.body;
           const filter = {
             email: userEmail,
           };
           const updatedDoc = {
             $set: {
-              balance: userBalance,
+              balance: parseFloat(userBalance.balance),
             },
           };
-          const result = await usersCollection.updateOne(filter, updatedDoc);
-          res.send(result);
+          console.log(userBalance)
+          // const result = await usersCollection.updateOne(filter, updatedDoc);
+          // res.send(result);
         });
     app.get("/v1/api/spotTrading", async (req, res) => {
       const result = await spotTradingCollection.find().toArray();
